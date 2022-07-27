f94d373a15158c37309dcd11fa8fd270a53a2c80
icchiCW <icchicw@gmail.com>
2022-07-27 11:50:37 +0900

    swiperテスト

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,24 @@ $('.js-click').on('click', function() {
   $(this).css('color', 'red');
 });
 
+var swiper = new Swiper('.swiper', {
+  autoplay: true,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'progressbar',
+    // type: 'bullets',
+    // clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
 import { hoge } from './hoge';
 hoge();
 
